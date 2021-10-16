9b4db46bcf79dac0f959b870d1ba7691e44b4e91
novacooo <jaceknowak226@gmail.com>
2021-10-16 19:57:46 +0200

    Backup

--- src/components/UsersListItem/UsersListItem.tsx
+++ src/components/UsersListItem/UsersListItem.tsx
@@ -1,21 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
 import { IUserData } from 'data/users';
+import { ReactComponent as DeleteIcon } from 'assets/icons/delete.svg';
 
 interface IUsersListItemProps {
   data: IUserData;
 }
 
-const UsersListItem = ({
-  data: { name, attendance, average },
-}: IUsersListItemProps) => (
-  <li>
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgrey;
+  }
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  width: 25px;
+  height: 25px;
+  background-color: #c0c7d6;
+  border: none;
+  border-radius: 50px;
+  color: #ffffff;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const StyledDeleteIcon = styled(DeleteIcon)`
+  width: 16px;
+  height: 16px;
+`;
+
+const UsersListItem = ({ data: { name, attendance, average } }: IUsersListItemProps) => (
+  <Wrapper>
     <div>{average}</div>
     <div>
       <p>{name}</p>
       <p>attendance {attendance}%</p>
     </div>
-    <button>X</button>
-  </li>
+    <StyledButton>
+      <StyledDeleteIcon />
+    </StyledButton>
+  </Wrapper>
 );
 
 export default UsersListItem;
--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,10 +1,23 @@
-import UsersList from 'components/UsersList/UsersList';
 import React from 'react';
+import styled from 'styled-components';
+import UsersList from 'components/UsersList/UsersList';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  padding: 50px;
+  width: 100%;
+  min-height: 100%;
+  background-color: #f7f8fa;
+`;
 
 const Root = () => (
-  <>
+  <Wrapper>
     <UsersList />
-  </>
+  </Wrapper>
 );
 
 export default Root;
