0ea6e0bbd9363ccc936ca9374f172687ae971ba8
novacooo <jaceknowak226@gmail.com>
2021-10-23 23:52:40 +0200

    Created FormField test

---
+++ src/components/molecules/FormField/FormField.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import FormField from 'components/molecules/FormField/FormField';
+import { renderWithThemeProvider } from 'helpers/RenderWithThemeProvider';
+
+describe('FormField component', () => {
+  it('Renders the component', () => {
+    renderWithThemeProvider(<FormField label="name" name="name" id="name" />);
+  });
+});
